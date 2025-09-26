4538768eb577ba4f6ed96c8ca5757fbbbb77afbc
AZhou <xiaoxiangyafei_1@163.com>
2025-09-26 03:28:47 +0000

    0926-2

--- scripts/data_process_0630/5_make_mlc_dataset.py
+++ scripts/data_process_0630/5_make_mlc_dataset.py
@@ -11,14 +11,15 @@ import cv2
 import numpy as np
 import argparse
 import glob
+from prettytable import PrettyTable
 from mmpretrain.structures import DataSample
 from cerwsi.nets import ValidClsNet
 from cerwsi.utils import KFBSlide,set_seed, init_distributed_mode, is_main_process
 
-WINDOW_SIZE = 1600
+WINDOW_SIZE = 800
 POSITIVE_CLASS = ['AGC', 'ASC-US','LSIL', 'ASC-H', 'HSIL']
 CLASS_COLORS = [[31,119,180], [255,153,153], [255,105,180], [255,20,147], [139,0,139]]
-max_negpatch_nums = 20    # 在每张 slide 中，阴性 tile 块的数量最多是 max_negpatch_nums 张, -1 则取所有
+max_negpatch_nums = 10    # 在每张 slide 中，阴性 tile 块的数量最多是 max_negpatch_nums 张, -1 则取所有
 max_try = 100   # 约束：neg_patch_thr <= max_try,  从 neg slide 中裁切 tile 块最多循环的次数
 data_root = f'data_resource/0630/WINDOW_SIZE_{WINDOW_SIZE}'
 inter_ann_dir = f'{data_root}/ann_jsons'
@@ -44,7 +45,7 @@ def cut_negslide():
     valid_model = valid_model.module
 
     data_group = {
-        # 'puretrain': 'data_resource/0630/4_pure_train.csv',
+        'puretrain': 'data_resource/0630/4_pure_train.csv',
         'val': 'data_resource/0630/6_val.csv'
     }
     for tag,csvpath in data_group.items():
@@ -115,112 +116,21 @@ def cut_negslide():
     torch.distributed.destroy_process_group()
 
 def concat_patchlist():
-    with open(f'{data_root}/ann_jsons/patches_in_RoI_pure_valid.json', 'r', encoding='utf-8') as f:
-        RoI_patchlist = json.load(f)
-    with open(f'{data_root}/ann_jsons/patches_in_RoI_jfsw_valid.json', 'r', encoding='utf-8') as f:
-        jfsw_pos_patchdata = json.load(f)
-    # with open(f'{data_root}/ann_jsons/patches_in_negslide_hs0.json', 'r', encoding='utf-8') as f:
-    #     negslide_patchlist = json.load(f)
-    
-    patient2patchlist = defaultdict(list)
-    for item in RoI_patchlist:
-        patient2patchlist[item['patientId']].append(item)
-    
-    data_group = {
-        'puretrain': 'data_resource/WINDOW_SIZE_1600/annofiles/4_pure_train.csv',
-        'val': 'data_resource/WINDOW_SIZE_1600/annofiles/6_val.csv'
-    }
-    for tag,csvpath in data_group.items():
-        multilabel_pn_cnt, binary_pn_cnt = [0,0],[0,0]
-        df_data = pd.read_csv(csvpath)
-        print(f'Load {tag} patchlist...')
-        patchlist = []
-        for row in tqdm(df_data.itertuples(index=False), total=len(df_data), ncols=80):
-            samplelist = patient2patchlist[row.patientId]
-            if tag == 'puretrain' and neg_patch_thr > 0:
-                poslist = [i for i in samplelist if i['diagnose']==1]
-                neglist = [i for i in samplelist if i['diagnose']==0]
-                random.shuffle(neglist)
-                samplelist = [*poslist, *neglist[:neg_patch_thr]]
-            patchlist.extend(samplelist)
-        
-        if tag == 'puretrain':
-            # patchlist.extend(negslide_patchlist)
-            patchlist.extend(jfsw_pos_patchdata)
-
-        multilabel_jsondata = {
-            "metainfo": {"classes": POSITIVE_CLASS},
-            "data_list": []
-        }
-        binarylabel_txtdata = []
-
-        print(f'Format {tag} patchlist to mmcls...')
-        for patchinfo in tqdm(patchlist, ncols=80):
-            imgname = f"{patchinfo['prefix']}/{patchinfo['filename']}"
-            if patchinfo['prefix'] == 'total_pos':
-                clsids = []
-                for i in patchinfo['clsnames']:
-                    if i == 'SCC':
-                        i = 'HSIL'
-                    clsids.append(POSITIVE_CLASS.index(i))
-
-                multilabel_jsondata['data_list'].append({
-                    "img_path": imgname,
-                    "gt_label": list(set(clsids))
-                })
-                multilabel_pn_cnt[patchinfo['diagnose']] += 1
-
-            binarylabel_txtdata.append(f'{imgname} {patchinfo["diagnose"]}\n')
-            binary_pn_cnt[patchinfo['diagnose']] += 1
-        
-        print(f'{tag} multilabel_pn_cnt: {multilabel_pn_cnt}')
-        print(f'{tag} binary_pn_cnt: {binary_pn_cnt}')
-
-        if tag == 'puretrain' and neg_patch_thr > 0:
-            tag += f'_npt{neg_patch_thr}'
-        with open(f'{ann_dir}/multilabel_{tag}_noneg.json', 'w', encoding='utf-8') as f:
-            json.dump(multilabel_jsondata, f, ensure_ascii=False)
-        # with open(f'{ann_dir}/binarylabel_{tag}.txt', 'w', encoding='utf-8') as f:
-        #     f.writelines(binarylabel_txtdata)
-
-def add_negslide():
-    pid_patchlist = defaultdict(list)
-    for filename in tqdm(os.listdir(f'{data_root}/images/neg_slide_rc'), ncols=80):
-        pid = filename.split('_rc')[0]
-        pid_patchlist[pid].append({
-            "img_path": f"neg_slide_rc/{filename}",
+    for tag in ['puretrain', 'val']:
+        with open(f'{data_root}/ann_jsons/multilabel_{tag}_nonegslide.json', 'r', encoding='utf-8') as f:
+            multilabel_data = json.load(f)
+        with open(f'{data_root}/ann_jsons/negslide_round0_{tag}.json', 'r', encoding='utf-8') as f:
+            negslide_data = json.load(f)
+        select_negpatches = [{
+            "img_path": f"{item['prefix']}/{item['filename']}",
             "gt_label": []
-        })
-        
-    with open(f'{data_root}/annofiles/multilabel_puretrain.json', 'r', encoding='utf-8') as f:
-        traindata = json.load(f)
-    with open(f'{data_root}/annofiles/multilabel_val.json', 'r', encoding='utf-8') as f:
-        valdata = json.load(f)
-    df_train = pd.read_csv(f'{data_root}/annofiles/45_purejfsw_train.csv')
-    df_val = pd.read_csv(f'{data_root}/annofiles/6_val.csv')
-    new_datalist = []
-    for row in tqdm(df_train.itertuples(index=False), total=len(df_train), ncols=80):
-        new_datalist.extend(pid_patchlist[row.patientId])
-    traindata['data_list'].extend(new_datalist)
-    with open(f'{data_root}/annofiles/multilabel_extendtrain.json', 'w', encoding='utf-8') as f:
-        json.dump(traindata, f, ensure_ascii=False)
-    pn_cnt = [0,0]
-    for datainfo in traindata['data_list']:
-        diag = int(len(datainfo['gt_label'])>0)
-        pn_cnt[diag] += 1
-    print(f'Train add {len(new_datalist)} patches, pn_cnt: {pn_cnt}')
-
-    new_datalist = []
-    for row in tqdm(df_val.itertuples(index=False), total=len(df_val), ncols=80):
-        new_datalist.extend(pid_patchlist[row.patientId])
-    valdata['data_list'].extend(new_datalist)
-    with open(f'{data_root}/annofiles/multilabel_extendval.json', 'w', encoding='utf-8') as f:
-        json.dump(valdata, f, ensure_ascii=False)
-    pn_cnt = [0,0]
-    for datainfo in valdata['data_list']:
-        diag = int(len(datainfo['gt_label'])>0)
-        pn_cnt[diag] += 1
-    print(f'Val add {len(new_datalist)} patches, pn_cnt: {pn_cnt}')
+        } for item in negslide_data]
+        # random.shuffle(select_negpatches)
+        # select_negpatches = select_negpatches[:int(len(select_negpatches)//2)]
+        multilabel_data['data_list'].extend(select_negpatches)
+        with open(f'{ann_dir}/multilabel_{tag}.json', 'w', encoding='utf-8') as f:
+            json.dump(multilabel_data, f, ensure_ascii=False)
+        print(f'{tag}: + {len(select_negpatches)} neg patches.')
 
 def extract_posslide():
     with open(f'{data_root}/ann_jsons/patches_in_RoI_pure_valid.json', 'r', encoding='utf-8') as f:
@@ -266,15 +176,15 @@ def extract_posslide():
         
         print(f'{tag} multilabel_pn_cnt: {multilabel_pn_cnt}')
 
-        with open(f'{inter_ann_dir}/multilabel_{tag}_noneg.json', 'w', encoding='utf-8') as f:
+        with open(f'{inter_ann_dir}/multilabel_{tag}_nonegslide.json', 'w', encoding='utf-8') as f:
             json.dump(multilabel_jsondata, f, ensure_ascii=False)
     
 def clear_imgs():
     keep_filename = []
     for tag in ['puretrain','val']:
-        with open(f'{inter_ann_dir}/negslide_round0_{tag}.json', 'r', encoding='utf-8') as f:
+        with open(f'{ann_dir}/multilabel_{tag}.json', 'r', encoding='utf-8') as f:
             json_data = json.load(f)
-        filenames = [item['filename'] for item in json_data]
+        filenames = [os.path.basename(item['img_path']) for item in json_data['data_list'] if 'neg_slide' in item['img_path']]
         keep_filename.extend(filenames)
     
     exists_imgpath = glob.glob(f'{neg_slide_img_savedir}/*.png')
@@ -284,46 +194,93 @@ def clear_imgs():
     #     filename = os.path.basename(imgpath)
     #     if filename not in keep_filename:
     #         os.remove(imgpath)
-    
+
+def statistic():
+    for tag in ['puretrain','val']:
+        with open(f'{ann_dir}/multilabel_{tag}.json', 'r', encoding='utf-8') as f:
+            json_data = json.load(f)
+        classes = json_data["metainfo"]["classes"]
+        data_list = json_data["data_list"]
+
+        # 每个类别 -> 出现过的 tile 数
+        label_count = defaultdict(int)
+        for item in data_list:
+            gt_labels = item.get("gt_label", [])
+            for label in gt_labels:
+                label_count[label] += 1
+
+        table = PrettyTable(title=tag)
+        table.field_names = classes
+        table.add_row([label_count.get(i, 0) for i in range(len(classes))])
+        print(table)
+
+
+
 if __name__ == "__main__":
     ann_dir = f'{data_root}/annofiles'
     os.makedirs(ann_dir, exist_ok=True, mode=0o777)
     # extract_posslide()
     # cut_negslide()
-    clear_imgs()
-    # partial_pos 样本只会用于阴阳二分类,不会用于多标签分类
-    # neg_patch_thr 只会作用于训练集，验证集保持不变（真实情况就是阳性 patch 远少于阴性 patch）
-    # concat_patchlist()
-    # add_negslide()
+    concat_patchlist()
+    # clear_imgs()
+    statistic()
 
 '''
-WS = 850
-neg_patch_thr = -1
-puretrain multilabel_pn_cnt: [51450, 12607]
-val multilabel_pn_cnt: [26437, 6584]
+WS = 1600, posslide max_negpatch_nums = 20; negslide max_negpatch_nums = 10
+puretrain multilabel_pn_cnt: [6172, 7396] + 11901 neg patches
+val multilabel_pn_cnt: [1531, 3818] + 2988 neg patches
++------------------------------------+
+|             puretrain              |
++-----+--------+------+-------+------+
+| AGC | ASC-US | LSIL | ASC-H | HSIL |
++-----+--------+------+-------+------+
+| 396 |  3139  | 2045 |  1912 | 2222 |
++-----+--------+------+-------+------+
++------------------------------------+
+|                val                 |
++-----+--------+------+-------+------+
+| AGC | ASC-US | LSIL | ASC-H | HSIL |
++-----+--------+------+-------+------+
+| 129 |  1677  | 868  |  938  | 1299 |
++-----+--------+------+-------+------+
 
-puretrain binary_pn_cnt: [51450, 47883]
-val binary_pn_cnt: [26437, 6616]
 
-neg_patch_thr = 5
-puretrain multilabel_pn_cnt: [5481, 12607] + [5987, 0] = [11468, 12607]
-puretrain binary_pn_cnt: [5481, 47883] + [5987, 0] = [11468, 47883]
+WS = 1200, max_negpatch_nums = 10
+puretrain multilabel_pn_cnt: [7607, 8929] + 11955 neg patches: [19562, 8929]
+val multilabel_pn_cnt: [1741, 4854] + 2990 neg patches: [4731, 4854]
++------------------------------------+
+|             puretrain              |
++-----+--------+------+-------+------+
+| AGC | ASC-US | LSIL | ASC-H | HSIL |
++-----+--------+------+-------+------+
+| 437 |  3336  | 2097 |  2244 | 2826 |
++-----+--------+------+-------+------+
++------------------------------------+
+|                val                 |
++-----+--------+------+-------+------+
+| AGC | ASC-US | LSIL | ASC-H | HSIL |
++-----+--------+------+-------+------+
+| 149 |  1934  | 940  |  1150 | 1687 |
++-----+--------+------+-------+------+
 
 
-WS = 1600, max_negpatch_nums = 20
-puretrain multilabel_pn_cnt: [6172, 7396]
-val multilabel_pn_cnt: [1531, 3818]
-
-
-Train add 7996 patches, pn_cnt: [20810, 7801]
-Val add 1495 patches, pn_cnt: [7709, 3916]
-
-puretrain binary_pn_cnt: [12814, 21030]
-val binary_pn_cnt: [6214, 3927]
-
-neg_patch_thr = 5
-puretrain multilabel_pn_cnt: [3665, 7801] + [5996, 0] = [9661, 7801]
-puretrain binary_pn_cnt: [3665, 21030] + [5996, 0] = [9661, 21030]
+WS = 800, max_negpatch_nums = 10
+puretrain multilabel_pn_cnt: [10737, 12672] + 11944 neg patches: [22681, 12672]
+val multilabel_pn_cnt: [2845, 6663] + 2990 neg patches: [5835, 6663]
++------------------------------------+
+|             puretrain              |
++-----+--------+------+-------+------+
+| AGC | ASC-US | LSIL | ASC-H | HSIL |
++-----+--------+------+-------+------+
+| 576 |  3979  | 2488 |  3043 | 4403 |
++-----+--------+------+-------+------+
++------------------------------------+
+|                val                 |
++-----+--------+------+-------+------+
+| AGC | ASC-US | LSIL | ASC-H | HSIL |
++-----+--------+------+-------+------+
+| 191 |  2172  | 1048 |  1612 | 2497 |
++-----+--------+------+-------+------+
 
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 torchrun  --nproc_per_node=8 --master_port=12342 scripts/data_process_0630/5_make_mlc_dataset.py
 '''
